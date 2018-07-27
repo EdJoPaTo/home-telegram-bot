4db8f0674122968f4b7fcc3d2c9d43d1f3832d1c
Edgar To <edjopato@gmail.com>
2018-07-27 18:17:33 +0200

    feat(graph): add menu to select (later) the settings

--- parts/graph.js
+++ parts/graph.js
@@ -7,31 +7,87 @@ const exec = util.promisify(require('child_process').exec)
 const format = require('../lib/format.js')
 const lastData = require('../lib/lastData.js')
 
+const { Extra, Markup } = Telegraf
+
 const DATA_PLOT_DIR = './tmp/'
 const DAY_IN_SECONDS = 60 * 60 * 24
-const DAYS_IN_GRAPH = 7
+const HOUR_IN_SECONDS = 60 * 60
+
+function calculateXMinFromTimeframe(timeframe) {
+  let match
+
+  if ((match = timeframe.match(/(\d+)d/))) {
+    const days = match[1]
+    return calculateXMinForDays(days)
+  } else if ((match = timeframe.match(/(\d+)h/))) {
+    const hours = match[1]
+    return calculateXMinForHours(hours)
+  } else {
+    return calculateXMinForDays(7)
+  }
+}
+
+function calculateXMinForDays(days) {
+  return Math.floor(Date.now() / 1000 / DAY_IN_SECONDS - (days - 1)) * DAY_IN_SECONDS
+}
+
+function calculateXMinForHours(hours) {
+  return Math.floor(Date.now() / 1000 / HOUR_IN_SECONDS - (hours - 1)) * HOUR_IN_SECONDS
+}
+
 
 const bot = new Telegraf.Composer()
 module.exports = bot
 
-bot.command('graph', async ctx => {
-  const tmpMsgPromise = ctx.reply('Die Graphen werden erstellt, habe einen Moment Geduld…')
+function generateKeyboardButtons(ctx) {
+  const buttons = []
 
   const positions = lastData.getPositions()
   const types = Object.keys(format.information)
 
-  await Promise.all(types.map(o => exec(createGnuplotCommandLine(o, positions))))
-  const mediaArr = types.map(o => ({media: { source: `${DATA_PLOT_DIR}${o}.png` }, type: 'photo'}))
+  // TODO: put in session
 
+  buttons.push([ Markup.callbackButton('Graph erstellen', 'g:create') ])
+
+  return buttons
+}
+
+bot.command('graph', ctx => {
+  const buttons = generateKeyboardButtons(ctx)
+
+  return ctx.reply(
+    'Wie möchtest du deinen Graphen haben?',
+    Extra.markup(Markup.inlineKeyboard(buttons))
+  )
+})
+
+bot.action('g:create', async ctx => {
+  ctx.editMessageText('Die Graphen werden erstellt, habe einen Moment Geduld…')
+
+  // TODO: get from session
+  const selectedSettings = {
+    types: ['temp', 'hum', 'rssi'],
+    positions: ['bude', 'bed', 'wt', 'rt'],
+    timeframe: '48h'
+  }
+
+  const { types, positions, timeframe } = selectedSettings
+
+  const xmin = calculateXMinFromTimeframe(timeframe)
+  await Promise.all(types.map(o => exec(createGnuplotCommandLine(o, positions, xmin))))
   ctx.replyWithChatAction('upload_photo')
-  await ctx.replyWithMediaGroup(mediaArr)
+  if (types.length > 1) {
+    const mediaArr = types.map(o => ({media: { source: `${DATA_PLOT_DIR}${o}.png` }, type: 'photo'}))
+    await ctx.replyWithMediaGroup(mediaArr)
+  } else {
+    await ctx.replyWithPhoto({ source: `${DATA_PLOT_DIR}${types[0]}.png` })
+  }
 
-  const tmpMsg = await tmpMsgPromise
-  return ctx.telegram.deleteMessage(ctx.chat.id, tmpMsg.message_id)
+  return ctx.deleteMessage()
 })
 
 // console.log('gnuplot commandline:', createGnuplotCommandLine('temp', ['bude', 'bed', 'books', 'rt', 'wt']))
-function createGnuplotCommandLine(type, positions) {
+function createGnuplotCommandLine(type, positions, xmin) {
   const typeInformation = format.information[type]
 
   const gnuplotParams = []
@@ -41,7 +97,6 @@ function createGnuplotCommandLine(type, positions) {
   gnuplotParams.push(`unit='${unit}'`)
   gnuplotParams.push(`type='${type}'`)
 
-  const xmin = Math.floor(Date.now() / 1000 / DAY_IN_SECONDS - (DAYS_IN_GRAPH - 1)) * DAY_IN_SECONDS
   gnuplotParams.push(`set xrange [${xmin}:*]`)
 
   // TODO: set DATA_PLOT_DIR in options in order to have always a seperate tmp dir for each plot
