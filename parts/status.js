8f8e099274daf619aedbd77e22b7f69a1eb31c5e
Edgar Toll <edjopato@gmail.com>
2019-01-28 15:40:01 +0100

    feat(status): don't filter out old entries
    If there is a problem with it it should be fixed instead of hidden.

--- parts/status.js
+++ parts/status.js
@@ -3,8 +3,6 @@ const Telegraf = require('telegraf')
 const format = require('../lib/format.js')
 const lastData = require('../lib/last-data.js')
 
-const AGE_HIDE = 1000 * 60 * 60 * 3 // 3h
-
 const bot = new Telegraf.Composer()
 module.exports = bot
 
@@ -15,13 +13,6 @@ bot.command('status', ctx => {
     const sensorData = lastData.getAllSensorValues(position)
     const types = Object.keys(sensorData)
       .filter(o => o !== 'connected')
-
-    const timestamps = types.map(type => sensorData[type].time)
-    const minAge = Date.now() - Math.max(...timestamps)
-
-    if (minAge > AGE_HIDE) {
-      return '' // Will be filtered out
-    }
 
     let parts = ''
     parts += format.connectionStatus(sensorData, {withText: false})
@@ -35,7 +26,6 @@ bot.command('status', ctx => {
 
     return parts
   })
-    .filter(o => o !== '')
 
   const text = lines.join('\n')
   return ctx.replyWithMarkdown(text)
