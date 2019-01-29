6734e4149c2cec6640c5c3d65cbe04c70c0fd549
Edgar Toll <edjopato@gmail.com>
2019-01-29 19:37:43 +0100

    feat(status): add selection for the types to be shown

--- parts/status.js
+++ parts/status.js
@@ -1,27 +1,37 @@
 const Telegraf = require('telegraf')
+const TelegrafInlineMenu = require('telegraf-inline-menu')
 
 const data = require('../lib/data')
 const format = require('../lib/format.js')
+const {getCommonPrefix} = require('../lib/mqtt-topic')
+const {toggleKeyInArray} = require('../lib/array-helper')
 
-const bot = new Telegraf.Composer()
+const menu = new TelegrafInlineMenu(getStatusText)
+menu.setCommand('status')
 
-bot.command('status', ctx => {
-  const positions = data.getPositions()
+function getSelectedTypes(ctx) {
+  return ((ctx.session.status || {}).types || data.getTypes())
+    .filter(o => o !== 'connected')
+}
+
+function getStatusText(ctx) {
+  const typesOfInterest = getSelectedTypes(ctx)
+
+  const positions = data.getPositions(o =>
+    Object.keys(o).some(type => typesOfInterest.indexOf(type) >= 0)
+  )
+  const commonPrefix = getCommonPrefix(positions)
 
   const lines = positions.map(position => {
     const types = data.getTypesOfPosition(position)
-      .filter(o => o !== 'connected')
-
-    if (types.length === 0) {
-      return ''
-    }
+      .filter(o => typesOfInterest.indexOf(o) >= 0)
 
     let parts = ''
     const connected = data.getLastValue(position, 'connected')
     parts += format.connectionStatus(connected && connected.value).emoji
     parts += ' '
 
-    parts += `*${position}*`
+    parts += `*${position.slice(commonPrefix.length)}*`
     parts += ' '
     parts += types.map(type =>
       format.typeValue(type, data.getLastValue(position, type).value)
@@ -29,11 +39,39 @@ bot.command('status', ctx => {
 
     return parts
   })
-    .filter(o => o)
 
-  const text = lines.join('\n')
-  return ctx.replyWithMarkdown(text)
+  let text = `*${commonPrefix}*\n`
+  text += lines.join('\n')
+  return text
+}
+
+function typeOptions() {
+  const allTypes = data.getTypes()
+    .filter(o => o !== 'connected')
+  const result = {}
+  allTypes.forEach(type => {
+    result[type] = format.information[type] ? format.information[type].label : type
+  })
+  return result
+}
+
+menu.select('type', typeOptions, {
+  columns: 2,
+  multiselect: true,
+  isSetFunc: (ctx, key) => getSelectedTypes(ctx).indexOf(key) >= 0,
+  setFunc: (ctx, key) => {
+    if (!ctx.session.status) {
+      ctx.session.status = {}
+    }
+
+    ctx.session.status.types = toggleKeyInArray(getSelectedTypes(ctx), key)
+  }
 })
+
+const bot = new Telegraf.Composer()
+bot.use(menu.init({
+  actionCode: 'status'
+}))
 
 module.exports = {
   bot
