7c4628062dddf599ee3a255802bb8f088001111f
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-14 09:09:41 +0200

    chore(install-systemd): assume user pi as default

--- install-systemd.sh
+++ install-systemd.sh
@@ -4,11 +4,6 @@ set -e
 nice npm ci --production
 nice npm run build
 
-echo
-echo WARNING
-echo Service will fail when some values are not filled out
-echo
-
 # systemd
 sudo cp -uv ./*.service /etc/systemd/system
 sudo systemctl daemon-reload
